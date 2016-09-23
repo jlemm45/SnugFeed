ec8cd488821844831a34774cbdbad5f876d81326
jlemm45 <jadenlemmon@gmail.com>
2016-09-22 20:22:02 -0500

    permissions

--- hooks/post-deploy.sh
+++ hooks/post-deploy.sh
@@ -6,5 +6,6 @@ php /var/www/snug/artisan view:clear
 php /var/www/snug/artisan cache:clear
 
 chmod -R 777 /var/www/snug/storage
+chmod -R 777 /var/www/snug/bootstrap
 
 composer install -d /var/www/snug
